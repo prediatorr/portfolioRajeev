ea5d23b33805def017daa511eb1369c03817acfb
prediatorr <rajeevkumarsingh5252@gmail.com>
2024-10-18 13:39:54 +0530

    PRGMAZ front added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./styles/App.scss";
 
 function App() {
-	return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>PRGMAZ</h1>
+        <h1 className="studio">STUDIO</h1>
+      </div>
+      <div className="arrow">
+        <svg id="eat6GYvfNiX1" viewBox="0 0 1000 1000">
+          <path
+            d="M200.872798,122.043256l259.429941.433216L628.50139,416.70822L357.237521,883.481109l-259.920489-.000001L369.345758,412.623077L200.872798,122.043256Z"
+            transform="translate(.000001 0.000002)"
+            stroke-width="2"
+          />
+          <path
+            d="M513.743263,122.476474l254.997321-.433216L936.699562,416.708222L768.740584,705.108719h-254.997321L682.833506,416.708222L513.743263,122.476474Z"
+            transform="translate(.000004 0)"
+            stroke-width="2"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 }
 
 export default App;
