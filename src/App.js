32f2fce1b66a62991ce692123bf02f518cae2677
Naman Baranwal <namanbaranwal2002@gmail.com>
2024-10-18 14:34:01 +0530

    Added Another section

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import "./styles/App.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="main">
-        <h1>PRGMAZ</h1>
-        <h1 className="studio">STUDIO</h1>
-      </div>
-      <div className="arrow">
-        <svg id="eat6GYvfNiX1" viewBox="0 0 1000 1000">
-          <path
-            d="M200.872798,122.043256l259.429941.433216L628.50139,416.70822L357.237521,883.481109l-259.920489-.000001L369.345758,412.623077L200.872798,122.043256Z"
-            transform="translate(.000001 0.000002)"
-            stroke-width="2"
-          />
-          <path
-            d="M513.743263,122.476474l254.997321-.433216L936.699562,416.708222L768.740584,705.108719h-254.997321L682.833506,416.708222L513.743263,122.476474Z"
-            transform="translate(.000004 0)"
-            stroke-width="2"
-          />
-        </svg>
-      </div>
-    </div>
-  );
+	useGSAP(() => {
+		ScrollTrigger.create({
+			trigger: ".App",
+			start: "top top",
+			end: "+=100%",
+			pin: ".home",
+			pinSpacing: false,
+		});
+
+		gsap.to(".works", {
+			scale: 1,
+			scrollTrigger: {
+				trigger: ".works",
+				start: "top center",
+				end: "end start",
+				scrub: true,
+			},
+		});
+	});
+
+	return (
+		<div className="container">
+			<div className="App">
+				<div className="home">
+					<div className="main">
+						<div className="title">
+							<h1 className="heading">Prgmaz</h1>
+							<h1 className="heading">Studio</h1>
+						</div>
+						<div className="arrow">
+							<svg id="eat6GYvfNiX1" viewBox="0 0 1000 1000">
+								<path
+									d="M200.872798,122.043256l259.429941.433216L628.50139,416.70822L357.237521,883.481109l-259.920489-.000001L369.345758,412.623077L200.872798,122.043256Z"
+									transform="translate(.000001 0.000002)"
+									stroke-width="2"
+								/>
+								<path
+									d="M513.743263,122.476474l254.997321-.433216L936.699562,416.708222L768.740584,705.108719h-254.997321L682.833506,416.708222L513.743263,122.476474Z"
+									transform="translate(.000004 0)"
+									stroke-width="2"
+								/>
+							</svg>
+						</div>
+					</div>
+					<div className="divider"></div>
+					<div className="slogan">
+						<div className="">
+							Your digital presence, beautifully
+						</div>
+						<div className="">designed and expertly built.</div>
+					</div>
+				</div>
+			</div>
+			<div className="works">Hello World!</div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Lenis from "lenis";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const lenis = new Lenis();
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+gsap.registerPlugin(ScrollTrigger);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
